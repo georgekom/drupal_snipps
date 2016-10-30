616278c51ea88091bb90d79e7d3e624980a1fcc2
George Kom <georgekom@users.noreply.github.com>
2016-10-30 12:00:01 +0200

    Re-added TABLE_SCHEMA conditional
    Not specifying the schema included all schemas.

--- db/cache-table-truncate.sql
+++ db/cache-table-truncate.sql
@@ -4,7 +4,8 @@ SELECT DISTINCT CONCAT(
   TABLE_NAME, 
   ";" ) 
 FROM INFORMATION_SCHEMA.TABLES
-WHERE TABLE_NAME LIKE  "cache%";
+WHERE TABLE_NAME LIKE  "cache%"
+AND TABLE_SCHEMA = 'dbname';
 
 -- use this to include db_name in statements (replace dbname)
 SELECT DISTINCT CONCAT(  
