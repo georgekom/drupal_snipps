039667edf8926895e6c5051aaca84bd7961e9c91
George Kom <georgekom@users.noreply.github.com>
2016-10-30 11:53:55 +0200

    remove underscore from tablename in LIKE

--- db/cache-table-truncate.sql
+++ db/cache-table-truncate.sql
@@ -1,5 +1,4 @@
--- use this to include db_name in statement (replace dbname)
-
+-- use this to include db_name in statements (replace dbname)
 SELECT DISTINCT CONCAT(  
   "TRUNCATE TABLE ", 
   TABLE_SCHEMA,  
@@ -7,5 +6,5 @@ SELECT DISTINCT CONCAT(
   TABLE_NAME, 
   ";" ) 
 FROM INFORMATION_SCHEMA.TABLES
-WHERE TABLE_NAME LIKE  "cache_%"
+WHERE TABLE_NAME LIKE  "cache%"
 AND TABLE_SCHEMA = 'dbname';
